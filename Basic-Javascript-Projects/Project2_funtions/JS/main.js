7cb992008cfbc6bdc0895f1d4a99434e9c1d2b10
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-06-21 10:52:21 -0600

    Created Project2
    These are the index.html and main.js files for Project2

---
+++ Basic-Javascript-Projects/Project2_funtions/JS/main.js
@@ -0,0 +1,6 @@
+function my_first_function() { //defining a funtion
+    var sentence="You clicked the button"; //concatenating a two  variables into a sentence
+    sentence += ", good job!";
+    document.getElementById("click_button").innerHTML = sentence; //creating the expression for the id
+}
+
