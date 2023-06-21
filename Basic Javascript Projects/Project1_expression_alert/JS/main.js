302553ddd1cd91ef3305eec3da34677726f524a1
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-06-21 09:51:35 -0600

    Created files for Project1_expressions_alert
    Created the index.html file and main.js file for Javascript Project1

---
+++ Basic Javascript Projects/Project1_expression_alert/JS/main.js
@@ -0,0 +1,15 @@
+var F="Hello,Dinosaur"; //Defining a variable with a string value
+window.alert(F); //creating an alert box containing the defined variable
+var X= "\"Dinosaurs are cool" +" and sometimes they are stinky\"- some 5 year old"; //Defining a variable with a concatenated string in quotes
+document.write(X); 
+
+document.write("Sometimes I dream" + " about dinosaurs."); //Creating a concatetenated string 
+
+var Herbivore = "tricerotops" , Carnivore = "trex", Omnivore="citipati"; //defining multiple variables
+document.write(Carnivore); //Getting the value of a defined variable
+
+document.write(3+4); //writing an expression
+
+var Sent1="This is the beginning of the string" // defining a variable
+var Sent2=" and this is the end of the string." //defining a variable
+document.write(Sent1+ Sent2) // writing an expression using the defined variables
