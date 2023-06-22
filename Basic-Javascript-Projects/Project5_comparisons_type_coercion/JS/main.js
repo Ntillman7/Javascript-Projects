b920d9ab9c4c76006ee4dc724a4ae18ada8acb97
Ntillman7 <135862581+Ntillman7@users.noreply.github.com>
2023-06-22 09:13:57 -0600

    Created Project5
    These are the index.html and main.js files for Project5_comparisons_type_coercion for the Tech Academy Bootcamp

---
+++ Basic-Javascript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,55 @@
+document.write(typeof "cat"); //displaying the type of variable
+
+document.write("13"+ 4); //combining a string and a number
+
+document.write("twelve" + 5); //combining a string and a number
+
+document.write("twelve"-3); //returning NaN
+
+document.write(isNaN('meow')); //showing that variable is not a number
+
+document.write(isNaN('3')); //showing that variable is a number
+
+document.write(5E310) //returning infinity
+
+document.write(-3E322) //returning -infinity
+
+document.write(55>2) //statement is true
+
+document.write(15<5) //statement is false
+
+console.log(2+2) //showing answer to equation in console
+
+console.log(78<56) //showing that comparison is false in console
+
+document.write(90==98) //these are not equal 
+
+document.write(90==90) //these are equal
+
+x=12;
+y=12;
+document.write(x===y); //these are same type and are equal value
+
+a="cat";
+b=12;
+document.write(a===b); // these are different type and different value
+
+h="12";
+j=12;
+document.write(h===j); //these are different type but same value
+
+y="horse";
+u="dog";
+document.write(y===u);//these are same type but differnt value
+
+document.write(9<13 && 10<89); //AND comparison returns true
+
+document.write(7<22 || 2>3); //OR comparison returns true
+
+document.write(3>2 && 4>5); //AND comparison returns false
+
+document.write(5>45 || 3>45); //OR comparison returns false
+
+document.write(!(20>10)); //comparison is not true 
+
+document.write(!(5>10)); //comparison is true
